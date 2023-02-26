24ad63573d00ceca3afd592d19ce05e0e8ceea71
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-02-26 12:54:04 +0100

    added search bar component and fixed error generated by the default elevation by setting it to zero

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
+import { Searchbar } from "react-native-paper";
+
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const onChangeSearch = (query) => setSearchQuery(query);
+
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <SearchBar />
+        <View style={styles.searchBar}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+            elevation={0}
+          />
+        </View>
         <ListView />
       </SafeAreaView>
       <ExpoStatusBar style="auto" />
     </>
-  );
-}
-function SearchBar() {
-  return (
-    <View style={styles.searchBar}>
-      <Text>Search</Text>
-    </View>
   );
 }
 function ListView() {
@@ -32,14 +37,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight,
+    backgroundColor: "#fff",
   },
   searchBar: {
-    padding: 16,
-    backgroundColor: "green",
+    margin: 16,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
   listView: {
     flex: 1,
     padding: 16,
-    backgroundColor: "blue",
   },
 });
