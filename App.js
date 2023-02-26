55efdc5e2b26569586f1b3c668a784d3527c3236
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-02-26 11:44:33 +0100

    completed the setup for eslint and prettier using the editors extensions capabilities

--- App.js
+++ App.js
@@ -1,44 +1,45 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
-
-
-const isAndroid = Platform.OS === 'android';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function App() {
   return (
-      <>
-        <SafeAreaView style={{flex:1, marginTop: StatusBar.currentHeight }}>
-          <SearchBar/>
-          <ListView/>
-        </SafeAreaView>
-        <ExpoStatusBar style="auto"/>
-      </>
-
+    <>
+      <SafeAreaView style={styles.container}>
+        <SearchBar />
+        <ListView />
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 function SearchBar() {
   return (
-      <View style={styles.searchBar}>
-        <Text>Search</Text>
-      </View>
+    <View style={styles.searchBar}>
+      <Text>Search</Text>
+    </View>
   );
 }
 function ListView() {
   return (
-      <View style={styles.listView}>
-        <Text>List</Text>
-      </View>
+    <View style={styles.listView}>
+      <Text>List</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
   searchBar: {
     padding: 16,
-    backgroundColor: 'green'
+    backgroundColor: "green",
   },
   listView: {
     flex: 1,
     padding: 16,
-    backgroundColor: 'blue'
-  }
+    backgroundColor: "blue",
+  },
 });
