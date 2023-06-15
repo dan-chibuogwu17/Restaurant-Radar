a5c520fc5e5236404294c3eecff25d34df46065c
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-06-15 01:16:18 +0100

    added custom fonts to the app and added the address field

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-//text
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import {
+  useFonts as useOswaldFonts,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import {
+  useFonts as useLatoFonts,
+  Lato_400Regular,
+} from "@expo-google-fonts/lato";
+
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldIsLoaded] = useOswaldFonts({
+    Oswald_400Regular,
+  });
+  const [latoIsLoaded] = useLatoFonts({
+    Lato_400Regular,
+  });
+
+  if (!oswaldIsLoaded || !latoIsLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
