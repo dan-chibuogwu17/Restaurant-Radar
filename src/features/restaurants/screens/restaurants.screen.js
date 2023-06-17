b436f12e594cc758073b9495ffdd3f433355d4ce
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-06-17 19:42:15 +0100

    refactored the spacer component because of the error it was throwing on android because of styled component we we were directly using as a component

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,19 +7,15 @@ import styled from "styled-components";
 const SafeArea = styled.SafeAreaView`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const SearchContainer = styled.View`
-  margin: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-  box-shadow: 2px 4px 7px lightgrey;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
-const SearchListContainer = styled.View`
+const RestaurantListContainer = styled.View`
   flex: 1;
   padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const RestaurantsScreen = () => {
@@ -32,12 +28,15 @@ export const RestaurantsScreen = () => {
           placeholder="Search"
           onChangeText={onChangeSearch}
           value={searchQuery}
-          elevation={0}
+          elevation={2}
+          style={{
+            backgroundColor: "#fff",
+          }}
         />
       </SearchContainer>
-      <SearchListContainer>
+      <RestaurantListContainer>
         <RestaurantInfoCard />
-      </SearchListContainer>
+      </RestaurantListContainer>
     </SafeArea>
   );
 };
