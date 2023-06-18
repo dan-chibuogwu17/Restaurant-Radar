b224c92cabe704f9bca94e1f4d15cab43e49642f
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-06-18 19:33:52 +0100

    working on flatlist and rendering a list of restaurants

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,36 +1,17 @@
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
-import { SvgXml } from "react-native-svg";
-import Star from "../../../../assets/Star";
-import Open from "../../../../assets/Open";
-import { Image } from "react-native";
+import {
+  Info,
+  Rating,
+  RestaurantCard,
+  SectionEnd,
+  Section,
+  Icon,
+  RestaurantCardCover,
+} from "./restaurant-info-card-styles";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Info = styled.View`
-  padding-top: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-`;
-const Section = styled.View`
-  flex-direction: row;
-  padding: ${(props) => props.theme.space[2]} 0;
-  justify-content: space-between;
-`;
-const SectionEnd = styled.View`
-  flex-direction: row;
-`;
+import { SvgXml } from "react-native-svg";
+import StarIcon from "../../../../assets/Star";
+import OpenIcon from "../../../../assets/Open";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -54,7 +35,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Section>
           <Rating>
             {ratingArray.map((_, i) => (
-              <SvgXml key={i} xml={Star} height={20} width={20} />
+              <SvgXml key={i} xml={StarIcon} height={20} width={20} />
             ))}
           </Rating>
           <SectionEnd>
@@ -62,14 +43,13 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
               <Text variant="error">CLOSED TEMPORARILY</Text>
             )}
             <Spacer position="left" size="large">
-              {isOPenNOw && <SvgXml xml={Open} height={20} width={20} />}
+              {isOPenNOw && <SvgXml xml={OpenIcon} height={20} width={20} />}
             </Spacer>
             <Spacer position="left" size="large">
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
-
         <Text variant="caption">{address}</Text>
       </Info>
     </RestaurantCard>
