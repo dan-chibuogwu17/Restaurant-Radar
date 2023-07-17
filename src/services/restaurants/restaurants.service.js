80cdd70bb4f27813c57a8001f477a79f27b49fcb
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2023-07-17 20:38:22 +0100

    added mock files for restaurants

---
+++ src/services/restaurants/restaurants.service.js
@@ -0,0 +1,8 @@
+import { mocks } from "./mock/index";
+//
+// export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+//   console.log(mocks);
+// };
+// restaurantsRequest();
+
+console.log(mocks, "daniel");
